7b61ccdf8ccd773ce8d261f0aa21bba90ac5f6ec
Nidhish Chauhan <rch246chauhan@gmail.com>
2023-04-11 01:57:18 +0530

    loader added

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,5 @@
+import gsap from "gsap";
+
 const bar = document.querySelector(".loading__bar--inner");
 const number = document.querySelector(".loading__counter--number");
 
@@ -10,5 +12,36 @@ let barInterval = setInterval(() => {
 
   if (cnt > 100) {
     clearInterval(barInterval);
+
+    gsap.to(".loading__bar", {
+      duration: 5,
+      rotate: "90deg",
+      opacity: 0,
+    });
+
+    gsap.to(".loading__counter, .loading__text", {
+      duration: 0.5,
+      opacity: 0,
+    });
+    gsap.to(".loading__box", {
+      duration: 1,
+      height: "500px",
+      borderRadius: "50%",
+    });
+    gsap.to(".loading__svg", {
+      duration: 10,
+      opacity: 1,
+    });
+    gsap.to(".loading__box", {
+      delay: 2,
+      border: "none",
+      duration: 0.5,
+    });
+    gsap.to(".loading", {
+      // duration: 2,
+      delay: 2,
+      background: "transparent",
+      opacity: 0.5,
+    });
   }
 }, 20);
